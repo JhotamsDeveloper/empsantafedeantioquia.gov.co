d3429c98b529d1e1d79222638aa0b15f473945f9
JhotaMSdeveloper <jhotams.developer@gmail.com>
2020-12-08 22:05:51 -0500

    Version fina configuran el Startup

--- src/prjESPSantaFeAnt/Startup.cs
+++ src/prjESPSantaFeAnt/Startup.cs
@@ -100,7 +100,7 @@ namespace prjESPSantaFeAnt
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
